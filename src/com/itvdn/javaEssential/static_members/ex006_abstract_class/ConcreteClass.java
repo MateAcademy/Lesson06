379006e0b749c084b178ba69c0777e029d48e0fa
s.klunniy <s.klunniy@gmail.com>
2022-05-12 19:44:44 +0300

    second commit

--- src/com/itvdn/javaEssential/static_members/ex006_abstract_class/ConcreteClass.java
+++ src/com/itvdn/javaEssential/static_members/ex006_abstract_class/ConcreteClass.java
@@ -1,6 +1,7 @@
 package com.itvdn.javaEssential.static_members.ex006_abstract_class;
 
 public class ConcreteClass extends AbstractClass {
+
     @Override
     public void Method() {
         System.out.println("Hello world!");
